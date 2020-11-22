fe0e7e31ea8636e46aa2614c150d326a039971ea
Luna Liu <53630450+lunalqk@users.noreply.github.com>
2020-11-22 13:53:33 -0800

    reupload

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms;
+
+public class CanvasManager : MonoBehaviour
+{
+    public static CanvasManager instance;
+
+    public PlayerController localPlayer;
+
+    [Header("Panels")]
+    [SerializeField] GameObject UI_Alive;
+    [SerializeField] GameObject UI_Death;
+	[SerializeField] GameObject UI_GameOver;
+
+
+    [Header("HUD")]
+    public TextMeshProUGUI AmmoCountText;
+    public TextMeshProUGUI HPText;
+    public Transform HPBar;
+
+    [Header("Death screen")]
+    public TextMeshProUGUI deathCount;
+    public TextMeshProUGUI killsCount;
+
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            //There's already existing canvas manager.
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        HideUI();
+    }
+
+    public void ChangePlayerState(bool isAlive)
+    {
+        UI_Alive.SetActive(isAlive);
+        UI_Death.SetActive(!isAlive);
+
+        //Update stats ui
+        if (!isAlive) {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+            deathCount.text = "Deaths: " + localPlayer.Deaths;
+            killsCount.text = "Kills: " + localPlayer.Kills;
+        }else{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+    }
+
+    public void HideUI()
+    {
+        UI_Alive.SetActive(false);
+        UI_Death.SetActive(false);
+		UI_GameOver.SetActive(false);
+    }
+
+    public void UpdateHP(int currentHP, int maxHP)
+    {
+        float curHPPerc = (float)currentHP / (float)maxHP;
+        HPBar.localScale = new Vector3(curHPPerc,1,1);
+
+        HPText.text = currentHP.ToString()+"/"+maxHP.ToString();
+    }
+	
+	//respawn button
+    public void RespawnBtn()
+    {
+		Debug.Log("Clicked Respawn");
+        if (localPlayer != null)
+            localPlayer.CmdRespawn();
+    }
+	
+	public void GameOverUI() {
+		HideUI();
+		UI_GameOver.SetActive(true);
+	}
+
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,364 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour {
+	
+	[Header("Player stats")]
+    [SyncVar] public int Health = 100;
+    [SyncVar] public int HealthMax = 100;
+    [SyncVar] public int Kills;
+    [SyncVar] public int Deaths;
+    [SyncVar] public bool isDead;
+
+	[Header("Player speed")]
+	[SerializeField] float PlayerSpeed;
+	[SerializeField] float PlayerSpeedMax;
+
+	[Header("Sensitivity")]
+	[SerializeField] float sensitivityX = 1f;
+	[SerializeField] float sensitivityY = 1f;
+	[SerializeField] float maxCameraX = 80f;
+	[SerializeField] float minCameraX = -80f;
+
+	[Header("Weapon")]
+	[SyncVar] int AmmoCountMax = 20;
+	[SyncVar] public int AmmoCount = 20;
+	[SyncVar] bool Reloading;
+	[SerializeField] double reloadTime = 2;
+	[SerializeField] int weaponDamage = 1;
+	[SerializeField] float WeaponCooldown;
+	
+	[Header("GFX")]
+    [SerializeField] GameObject[] disableOnClient;
+    [SerializeField] GameObject[] disableOnDeath;
+
+	[Header("Camera")]
+	[SerializeField] Vector3 cameraOffset;
+
+	[Header("Debug")]
+	[SerializeField] bool DebugMode = false;
+
+
+	Rigidbody rb;
+	float xRotation;
+	float curCooldown;
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody>();
+		
+		if (isLocalPlayer) {
+
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+
+			//Setup FPS camera.
+			Camera.main.transform.SetParent(transform);
+			Camera.main.transform.localPosition = cameraOffset;
+			Camera.main.transform.rotation = Quaternion.identity;
+			
+			CanvasManager.instance.ChangePlayerState(true);
+			CanvasManager.instance.UpdateHP(Health , HealthMax);
+			CanvasManager.instance.localPlayer = this;
+			CanvasManager.instance.AmmoCountText.text = AmmoCount.ToString() + "/" + AmmoCountMax.ToString();
+			
+			foreach (GameObject go in disableOnClient) {
+                go.SetActive(false);
+            }
+		
+		} else {
+			rb.isKinematic = true;
+		}
+	}
+		
+	void Update () {
+		if (!isLocalPlayer) return;
+
+		if (DebugMode) {
+			Camera.main.transform.localPosition = cameraOffset;
+		}
+		
+		curCooldown -= Time.deltaTime;
+		
+		if (!isDead) {
+			if (Input.GetKey("w")||Input.GetKey("a")||Input.GetKey("s")||Input.GetKey("d")) {
+				anim.SetBool("isRunning", true);
+				MoveInput();
+			} else {
+				anim.SetBool("isRunning", false);
+			}
+			
+			MouseLook();
+
+			if (Input.GetMouseButtonDown(0)) {
+				ShootButton();
+			}
+			
+			if(Input.GetKeyDown("r")) {
+				ReloadButton();
+			}	
+		}
+		
+		if (Kills >= 2) {
+			GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+			
+			foreach (GameObject p in players) {
+				p.GetComponent<PlayerController>().isDead = true;
+				p.GetComponent<PlayerController>().CmdEndGame();
+			}
+		}
+		
+	}
+
+
+	private void MoveInput() {
+		//Input
+		if (Input.GetAxis("Horizontal") != Mathf.Epsilon || Input.GetAxis("Vertical") != Mathf.Epsilon)	{
+			Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			movementDirection *= PlayerSpeed;
+			movementDirection = Vector3.ClampMagnitude(movementDirection, PlayerSpeed);
+
+			if (rb.velocity.magnitude < PlayerSpeedMax)
+				rb.AddRelativeForce(movementDirection * Time.deltaTime * 100);
+		}
+	}
+
+	private void MouseLook() {
+		float rotY = Input.GetAxis ("Mouse X") * sensitivityY;
+		float rotX = -Input.GetAxis ("Mouse Y") * sensitivityX;
+
+		//Body rotation
+		transform.Rotate(0, rotY, 0);
+
+		//Camera rotation
+		xRotation = Mathf.Clamp(xRotation + rotX, minCameraX, maxCameraX);
+		//Debug.Log("" + xRotation);
+		//weaponArm.localEulerAngles = new Vector3(xRotation, 0, 0);
+		Camera.main.transform.localEulerAngles = new Vector3(xRotation, 0, 0);
+	}
+	
+	
+	
+	
+	
+	
+	//
+	// SHOOTING
+	//
+	internal void ShootButton() {
+        //First local if can shoot check.
+        //if ammoCount > 0 && isAlive
+        if (Reloading == false && AmmoCount > 0 && !isDead && curCooldown < 0.01f) {
+            //Do command
+            CmdTryShoot(Camera.main.transform.forward, Camera.main.transform.position);
+            curCooldown = WeaponCooldown;
+        }
+    }
+	
+	[Command]
+	void CmdTryShoot(Vector3 clientCam, Vector3 clientCamPos) {
+        //Server side check
+        //if ammoCount > 0 && isAlive
+        if (AmmoCount > 0 && !isDead) {
+            AmmoCount--;
+            TargetShoot();
+
+            Ray ray = new Ray(clientCamPos, clientCam * 500);
+            Debug.DrawRay(clientCamPos, clientCam * 500, Color.red, 2f);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+
+                Debug.Log("SERVER: Player shot: " + hit.collider.name);
+                if (hit.collider.CompareTag("Player") && hit.collider.GetComponent<PlayerController>().isDead==false) {
+                    //RpcPlayerFiredEntity(GetComponent<NetworkIdentity>().netId, hit.collider.GetComponent<NetworkIdentity>().netId, hit.point, hit.normal);
+                    hit.collider.GetComponent<PlayerController>().Damage(weaponDamage, GetComponent<NetworkIdentity>().netId);
+                }
+                else {
+                    //RpcPlayerFired(GetComponent<NetworkIdentity>().netId, hit.point, hit.normal);
+
+                }
+
+            }
+        }
+
+    }
+	
+	[TargetRpc]
+    void TargetShoot() {
+        //We shot successfully.
+        //Update UI
+        CanvasManager.instance.AmmoCountText.text = AmmoCount.ToString() + "/" + AmmoCountMax.ToString();
+    }
+	
+	[ClientRpc]
+    void RpcPlayerFired(uint shooterID, Vector3 impactPos, Vector3 impactRot) {
+        //Instantiate(bulletHolePrefab, impactPos + impactRot * 0.1f, Quaternion.LookRotation(impactRot));
+        //Instantiate(bulletFXPrefab, impactPos, Quaternion.LookRotation(impactRot));
+        //NetworkIdentity.spawned[shooterID].GetComponent<PlayerController>().MuzzleFlash();
+    }
+	
+	[ClientRpc]
+    void RpcPlayerFiredEntity(uint shooterID, uint targetID, Vector3 impactPos, Vector3 impactRot) {
+        //Instantiate(bulletHolePrefab, impactPos + impactRot * 0.1f, Quaternion.LookRotation(impactRot), NetworkIdentity.spawned[targetID].transform);
+        //Instantiate(bulletBloodFXPrefab, impactPos, Quaternion.LookRotation(impactRot));
+        //NetworkIdentity.spawned[shooterID].GetComponent<PlayerController>().MuzzleFlash();
+
+    }
+	
+	[Server]
+    public void Damage(int amount, uint shooterID) {
+        Health -= amount;
+        TargetGotDamage();
+        if (Health < 1) {
+            Die();
+            NetworkIdentity.spawned[shooterID].GetComponent<PlayerController>().Kills++;
+            NetworkIdentity.spawned[shooterID].GetComponent<PlayerController>().TargetGotKill();
+        }
+    }
+	
+	[TargetRpc]
+    public void TargetGotKill() {
+        Debug.Log("You got kill.");
+    }
+	
+	[TargetRpc]
+    public void TargetGotDamage() {
+        CanvasManager.instance.UpdateHP(Health, HealthMax);
+        Debug.Log("We got hit!");
+    }
+	
+	[Server]
+    public void Die() {
+        Deaths++;
+        isDead = true;
+        Debug.Log("SERVER: Player died.");
+        TargetDie();
+        //RpcPlayerDie();
+    }
+	
+	[TargetRpc]
+    void TargetDie() {
+        //Called on the died player.
+        CanvasManager.instance.ChangePlayerState(!isDead);
+        Debug.Log("You died.");
+    }
+	
+	[ClientRpc]
+    void RpcPlayerDie() {
+        GetComponent<Collider>().enabled = false;
+        foreach (GameObject item in disableOnDeath)
+        {
+            item.SetActive(false);
+        }
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//
+	//RELOADING
+	//
+	[Client]
+    internal void ReloadButton() {
+        if(Reloading || AmmoCount != AmmoCountMax)
+            CmdTryReload();
+    }
+
+    [Command]
+    void CmdTryReload() {
+        if (Reloading || AmmoCount == AmmoCountMax)
+            return;
+
+        StartCoroutine(reloadingWeapon());
+    }
+
+    IEnumerator reloadingWeapon() {
+        Reloading = true;
+        yield return new WaitForSeconds((float)reloadTime);
+        AmmoCount = AmmoCountMax;
+        TargetReload();
+        Reloading = false;
+
+        yield return null;
+    }
+	
+	[TargetRpc]
+    void TargetReload()
+    {
+        //We reloaded successfully.
+        //Update UI
+        CanvasManager.instance.AmmoCountText.text = AmmoCount.ToString() + "/" + AmmoCountMax.ToString();
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//
+	// RESPAWN
+	//
+	[Command]
+    public void CmdRespawn() {
+        //Check if dead
+        if (isDead) {
+            Health = HealthMax;
+            AmmoCount = AmmoCountMax;
+            isDead = false;
+            TargetRespawn();
+            RpcPlayerRespawn();
+        }
+    }
+	
+	[TargetRpc]
+    void TargetRespawn() {
+        CanvasManager.instance.ChangePlayerState(true);
+		CanvasManager.instance.UpdateHP(Health, HealthMax);
+        //set position
+        //transform.position = NetworkManagerFPS.singleton.GetStartPosition().position;
+
+    }
+	
+	[ClientRpc]
+    void RpcPlayerRespawn() {
+        GetComponent<Collider>().enabled = true;
+
+        foreach (GameObject item in disableOnDeath) {
+            item.SetActive(true);
+        }
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	[Command]
+	public void CmdEndGame(){
+		CanvasManager.instance.GameOverUI();
+	}
+	
+}
