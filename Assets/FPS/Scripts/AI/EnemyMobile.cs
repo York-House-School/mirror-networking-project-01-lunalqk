40882ac8b772b93df318fdd4e66e2f71cb65feaa
Luna Liu <lunaliu11082004@gmail.com>
2022-03-05 17:09:46 -0800

    fixed some errors, and added level 3

--- Assets/FPS/Scripts/AI/EnemyMobile.cs
+++ Assets/FPS/Scripts/AI/EnemyMobile.cs
@@ -73,8 +73,8 @@ namespace Unity.FPS.AI
             Animator.SetFloat(k_AnimMoveSpeedParameter, moveSpeed);
 
             // changing the pitch of the movement sound depending on the movement speed
-            m_AudioSource.pitch = Mathf.Lerp(PitchDistortionMovementSpeed.Min, PitchDistortionMovementSpeed.Max,
-                moveSpeed / m_EnemyController.NavMeshAgent.speed);
+            //m_AudioSource.pitch = Mathf.Lerp(PitchDistortionMovementSpeed.Min, PitchDistortionMovementSpeed.Max,
+                //moveSpeed / m_EnemyController.NavMeshAgent.speed);
         }
 
         void UpdateAiStateTransitions()
